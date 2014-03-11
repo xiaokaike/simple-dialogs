846fb716a95add3f041d581bef11ca187ef94d2b
xiaokaike <kexiaokai@gmail.com>
2014-03-11 15:37:36 +0800

    with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+///*global module:false*/
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+
+    // less : {
+    //   dev : {
+    //     src : 'assets/styles/main.less',
+    //     dest : 'assets/styles/main.css'
+    //   },
+    //   dist : {
+    //     options :{
+    //       yuicompress : true,
+    //       report : 'gzip'
+    //     },
+    //     src : '<%= less.dev.src %>',
+    //     dest : '<%= less.dev.dest %>'
+    //   }
+    // },
+
+    // concat : {
+    //   dist : {
+    //     src : [
+    //       'assets/scripts/vendor/*.js',
+    //       'assets/scripts/examples/CodeGenerator.js',
+    //       'assets/scripts/examples/Playground.js'
+    //     ],
+    //     dest : 'assets/scripts/main.js'
+    //   }
+    // },
+
+    // uglify : {
+    //   options : {
+    //     report: 'gzip'
+    //   },
+    //   dist : {
+    //     src: '<%= concat.dist.dest %>',
+    //     dest: '<%= concat.dist.dest %>'
+    //   }
+    // },
+
+    // jshint : {
+    //   options : {
+    //     jshintrc : '.jshintrc'
+    //   },
+    //   files : [
+    //     'Gruntfile.js',
+    //     'assets/scripts/examples/*.js'
+    //   ]
+    // },
+
+    connect: {
+      server: {
+        options : {
+          port : 8001,
+          open : true,
+          debug : true
+        }
+      }
+    },
+
+    watch : {
+      options : {
+        atBegin : true
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('dist', ['less:dist', 'jshint', 'concat', 'uglify']);
+};
